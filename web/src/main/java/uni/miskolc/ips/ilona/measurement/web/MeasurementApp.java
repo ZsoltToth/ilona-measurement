ac5f6ec0b992f175ee4a05ad9f54eb70d71534ff
Zsolt Toth <tothzs87@gmail.com>
2020-05-27 11:41:41 +0200

    Spring Boot application starts with command line arguments.
    See MeasurementApp.java for command line arguments for JVM.
    
    #27

--- web/src/main/java/uni/miskolc/ips/ilona/measurement/web/MeasurementApp.java
+++ web/src/main/java/uni/miskolc/ips/ilona/measurement/web/MeasurementApp.java
@@ -6,6 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MeasurementApp {
 
+    /**
+     *
+     * -Ddatabase.host=127.0.0.1
+     * -Ddatabase.port=3306
+     * -Ddatabase.db=Ilona
+     * -Ddatabase.user=ilona
+     * -Ddatabase.password=secret
+     * -Dmybatis.config=<path-to-persist-mysql>/persist.mysql/src/main/resources/mybatis-configuration.xml
+     *
+     * @param args
+     */
     public static void main(String[] args){
         SpringApplication.run(MeasurementApp.class,args);
     }
