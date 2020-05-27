356c2143173a8c4f57c7e1797f5e7222dbd3f04c
Zsolt Toth <tothzs87@gmail.com>
2020-05-27 11:29:07 +0200

    SLF4J dependencies are fixed.
    Spring-context should be removed. Spring boot will import everything.
    
    #27

---
+++ web/src/main/java/uni/miskolc/ips/ilona/measurement/web/MeasurementApp.java
@@ -0,0 +1,12 @@
+package uni.miskolc.ips.ilona.measurement.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeasurementApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(MeasurementApp.class,args);
+    }
+}
