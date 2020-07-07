8ea6e75a45b2c1163116c87bea944c5de3ae4952
Zsolt Toth <tothzs87@gmail.com>
2020-07-07 07:56:47 +0200

    import-dataset.sh has been fixed to root create the ilona user.
    The ilona user tried to create itself which caused problem. The root user of the db creates ilona.

--- persist.mysql/src/resources/import-dataset.sh
+++ persist.mysql/src/resources/import-dataset.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-mysql -h 127.0.0.1 -u ilona -psecret < ./create-user.sql
+mysql -h 127.0.0.1 -u root  -psecret < ./create-user.sql
 mysql -h 127.0.0.1 -u ilona -psecret < ./dataset.sql
