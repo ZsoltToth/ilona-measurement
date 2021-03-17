0fab0d4a7e437c5b8928e66f05558e47c681ed39
Norbert Harangozó <maszek.solutions@gmail.com>
2021-03-17 14:05:59 +0100

    refactor(#51): fix NonEmptyAtclauseDescription checkstyle violation

--- src/main/java/uni/miskolc/ips/ilona/measurement/web/MeasurementApp.java
+++ src/main/java/uni/miskolc/ips/ilona/measurement/web/MeasurementApp.java
@@ -9,8 +9,6 @@ public class MeasurementApp {
     /**
      * -Ddatabase.host=127.0.0.1 -Ddatabase.port=3306 -Ddatabase.db=Ilona -Ddatabase.user=ilona
      * -Ddatabase.password=secret
-     *
-     * @param args
      */
     public static void main(String[] args) {
         SpringApplication.run(MeasurementApp.class, args);
