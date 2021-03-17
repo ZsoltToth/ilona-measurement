384471d62b9e4e075ca12cd8ad3f80294b60ab7b
Norbert Harangozó <maszek.solutions@gmail.com>
2021-03-17 15:16:57 +0100

    refactor: replace equals method with Lombok annotation in the Measurement class
    Also removed the unnecessary comments from this class.

--- src/main/java/uni/miskolc/ips/ilona/measurement/model/measurement/Measurement.java
+++ src/main/java/uni/miskolc/ips/ilona/measurement/model/measurement/Measurement.java
@@ -1,5 +1,6 @@
 package uni.miskolc.ips.ilona.measurement.model.measurement;
 
+import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import uni.miskolc.ips.ilona.measurement.model.position.Position;
 
@@ -14,70 +15,29 @@ import java.util.UUID;
  * @author zsolt
  */
 @ToString
+@EqualsAndHashCode
 public class Measurement {
 
-    /**
-     * It represents a universally unique identifier, which identifies the measurement.
-     */
     private UUID id;
 
-    /**
-     * The class Date represents a specific instant in time, with millisecond precision.
-     */
     private Date timestamp;
 
-    /**
-     * The Position class.
-     */
     private Position position;
 
-    // Measured Values
-
-    /**
-     * Received signal strength indicator is a measurement of the power present in a received radio
-     * signal.
-     */
     private WifiRssi wifiRssi;
 
-    /**
-     * It represents the measured values of a Magnetometer. The magnetometer returns values in
-     * Cartesian coordinate system.
-     */
     private Magnetometer magnetometer;
 
-    /**
-     * Represents the measurement of the bluetooth sensor and calculates the distance of two bluetooth
-     * sensor measurement.
-     */
     private BluetoothTags bluetoothTags;
 
-    /**
-     * The class responsible for the GPS Coordinate of the measurement.
-     */
     private GpsCoordinate gpsCoordinates;
 
-    /**
-     * The RFIDTags class.
-     */
     private RfidTags rfidtags;
 
-    /**
-     * Constructor for the Measurement class.
-     */
     public Measurement() {
         super();
     }
 
-    /**
-     * @param timestamp      represents a specific instant in time, with millisecond.
-     * @param position       based on the coordinate, zone and UUID attributes.
-     * @param wifiRssi       represents the received signal strength indicator.
-     * @param magnetometer   measured values.
-     * @param bluetoothTags  represents the measurement of the bluetooth sensor.
-     * @param gpsCoordinates defines the latitude, longitude and distance.
-     * @param rfidTags       uses electromagnetic fields to automatically identify and track tags attached
-     *                       to objects.
-     */
     protected Measurement(
             final Date timestamp,
             final Position position,
@@ -97,156 +57,67 @@ public class Measurement {
         this.rfidtags = rfidTags;
     }
 
-    /**
-     * RFID tag getter method.
-     *
-     * @return the RFID tags.
-     */
     public final RfidTags getRfidtags() {
         return rfidtags;
     }
 
-    /**
-     * RFID tag setter method.
-     *
-     * @param rfidtags sets the value of rfidtags.
-     */
     public final void setRfidtags(final RfidTags rfidtags) {
         this.rfidtags = rfidtags;
     }
 
-    /**
-     * WiFiRSSI getter method.
-     *
-     * @return wifiRSSI gets the measured WiFiRSSI values.
-     */
     public final WifiRssi getWifiRssi() {
         return wifiRssi;
     }
 
-    /**
-     * WiFiRSSI setter method.
-     *
-     * @param wifiRssi sets the measured WiFIRSSI values.
-     */
     public final void setWifiRssi(final WifiRssi wifiRssi) {
         this.wifiRssi = wifiRssi;
     }
 
-    /**
-     * Magnetometer getter method.
-     *
-     * @return magnetometer values in Cartesian coordinate system.
-     */
     public final Magnetometer getMagnetometer() {
         return magnetometer;
     }
 
-    /**
-     * Magnetometer setter method.
-     *
-     * @param magnetometer sets magnetometer values.
-     */
     public final void setMagnetometer(final Magnetometer magnetometer) {
         this.magnetometer = magnetometer;
     }
 
-    /**
-     * BluetoothTags getter method.
-     *
-     * @return bluetoothTags gets the measured and distance data.
-     */
     public final BluetoothTags getBluetoothTags() {
         return bluetoothTags;
     }
 
-    /**
-     * BluetoothTags setter method.
-     *
-     * @param bluetoothTags sets the measured and distance data.
-     */
     public final void setBluetoothTags(final BluetoothTags bluetoothTags) {
         this.bluetoothTags = bluetoothTags;
     }
 
-    /**
-     * GPSCoordinate getter method.
-     *
-     * @return gpsCoordinates gets the calculated coordinates.
-     */
     public final GpsCoordinate getGpsCoordinates() {
         return gpsCoordinates;
     }
 
-    /**
-     * GpsCoordinates setter method.
-     *
-     * @param gpsCoordinates sets the calculated coordinates.
-     */
     public final void setGpsCoordinates(final GpsCoordinate gpsCoordinates) {
         this.gpsCoordinates = gpsCoordinates;
     }
 
-    /**
-     * UUID getter method.
-     *
-     * @return id gets the measurement identifier.
-     */
     public final UUID getId() {
         return id;
     }
 
-    /**
-     * UUID setter method.
-     *
-     * @param id sets the measurement identifier.
-     */
     public final void setId(final UUID id) {
         this.id = id;
     }
 
-    /**
-     * Date getter method.
-     *
-     * @return timestamp gets the current timestamp measured in milliseconds.
-     */
     public final Date getTimestamp() {
         return timestamp;
     }
 
-    /**
-     * Timestamp setter method.
-     *
-     * @param timestamp sets the current timestamp measured in milliseconds.
-     */
     public final void setTimestamp(final Date timestamp) {
         this.timestamp = timestamp;
     }
 
-    /**
-     * Position getter method.
-     *
-     * @return position gets the position based on the coordinate, zone and UUID attributes.
-     */
     public final Position getPosition() {
         return position;
     }
 
-    /**
-     * Position setter method.
-     *
-     * @param position sets the position based on the coordinate, zone and UUID attributes.
-     */
     public final void setPosition(final Position position) {
         this.position = position;
-    }
-
-    @Override
-    public final boolean equals(final Object obj) {
-        if (!(obj instanceof Measurement)) {
-            return false;
-        }
-
-        return this.id.equals(((Measurement) obj).id);
     }
 }
